45b3fa779ad5027950a6d2581057f7276bd3a347
Mohammad <alisafamohammadi785@gmail.com>
2026-05-16 19:14:53 +0430

    Changes added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/product/create', 'Product.create');
+Route::post('/product/add', [ProductController::class, 'create']);
+Route::get('product', [ProductController::class, 'index']);
