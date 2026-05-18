5c16462b6add0e5696867e71f41544332cb9f549
Mohammad <alisafamohammadi785@gmail.com>
2026-05-18 19:42:01 +0430

    Changes added

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,9 @@ Route::get('product', [ProductController::class, 'index']);
 
 Route::get("Sinf", [SinfController::class, "index"]);
 Route::get("Student", [StudentController::class, "index"]);
+
+Route::post("create-post",[PostController::class, "create"]);
+Route::get("Post",[PostController::class, "index"]);
+Route::view("Post/create", 'Post.create');
+Route::put('Post/update/{id}', [PostController::class, 'update']);
+Route::get('Post/edit/{id}', [PostController::class, 'edit']);
